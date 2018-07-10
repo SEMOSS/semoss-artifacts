58fa5418c47a0c2b79b6d7f9182310ba2eee853d
Timothy <tbanach@deloitte.com>
2018-07-10 14:39:52 +0000

    UPDATE ::: progress so far on script

--- artifacts/scripts/update_latest_dev.sh
+++ artifacts/scripts/update_latest_dev.sh
@@ -11,9 +11,15 @@ fi
 
 echo latest version is $latest_version
 echo current version is $version
- 
-if [ "$#" -gt 0 ] && [[ (( $latest_version > $version )) ]]; then
-        version=$latest_version
+
+# If the version is being overridden, or the latest version is greater than the current, then update
+if [ "$#" -gt 0 ] || [[ (( $latest_version > $version )) ]]; then
+        # Always use the overridden version if provided 
+        if [ "$#" -gt 0 ]; then
+                version=$1
+        else 
+                version=$latest_version
+        fi
         echo "Updating to version.. $version"
         cd /opt/semosshome/semoss-artifacts
         git pull
@@ -24,9 +30,9 @@ if [ "$#" -gt 0 ] && [[ (( $latest_version > $version )) ]]; then
         cd /opt/semosshome/semoss-artifacts/artifacts/war && mvn clean install -Dci.version=$version
         cp -r /opt/semosshome/semoss-artifacts/artifacts/war/monolith*/* /opt/apache-tomcat-8.0.41/webapps/Monolith
         cd /opt/semosshome/semoss-artifacts/artifacts/lib && mvn clean install -Dci.version=$version
-        cp -r /opt/semosshome/semoss-artifacts/artifacts/lib/monolith*/* /opt/apache-tomcat-8.0.41/webapps/Monolith
+        cp -r /opt/semosshome/semoss-artifacts/artifacts/lib/monolith*/WEB-INF/lib/* /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/lib/
                              cp -r /opt/semosshome/semoss-artifacts/x/RDF_Map.prop /opt/semosshome 
-                             cp -r /opt/semosshome/semoss-artifacts/x/web.xml /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF 
+                             cp -r /opt/semosshome/semoss-artifacts/x/web.xml /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/web.xml 
         echo "version=$latest_version" > /opt/semosshome/semoss-artifacts/ver.txt
 else
         echo "Semoss is already up to date"
