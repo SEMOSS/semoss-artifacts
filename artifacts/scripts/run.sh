a75b4477d83506923ee7ebd81919f7ee19f029d1
Banach <tbanach@deloitte.com>
2018-07-30 16:00:19 -0400

    UPDATE ::: adding run scripts

---
+++ artifacts/scripts/run.sh
@@ -0,0 +1,2 @@
+cd /opt/apache-tomcat-8.0.41/bin
+./catalina.sh run
---
+++ artifacts/scripts/run_update_all.sh
@@ -0,0 +1,4 @@
+cd /opt/semosshome/semoss-artifacts/artifacts/scripts
+./update_latest_dev.sh
+cd /opt/apache-tomcat-8.0.41/bin
+./catalina.sh run
