41604584e7c7af6071a690ef9cfcd1495bec24bc
Banach <tbanach@deloitte.com>
2018-08-01 16:40:40 -0400

    BUGFIX ::: chmod on updated scripts

--- artifacts/scripts/run_update.sh
+++ artifacts/scripts/run_update.sh
@@ -1,5 +1,8 @@
 cd /opt/semoss-artifacts
+# Because chmod looks like a chage to git
+git checkout .
 git pull
+chmod 777 /opt/semoss-artifacts/artifacts/scripts/*
 cd /opt/semoss-artifacts/artifacts/scripts
 ./update_latest_dev_no_lib.sh
 cd /opt/apache-tomcat-8.0.41/bin
