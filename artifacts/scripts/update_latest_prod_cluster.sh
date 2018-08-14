5f6a21b17f71141168092209acc878d50232db98
Banach <tbanach@deloitte.com>
2018-08-14 17:08:57 -0400

    update : scripts to pull latest cluster jar

---
+++ artifacts/scripts/update_latest_prod_cluster.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+latest_version=`curl -s "https://oss.sonatype.org/content/groups/public/org/semoss/semosscluster/maven-metadata.xml" | grep "<release>.*</release>" | sed -e "s#\(.*\)\(<release>\)\(.*\)\(</release>\)\(.*\)#\3#g"`
+ 
+if [ -f /opt/semoss-artifacts/ver.txt ]; then
+        source /opt/semoss-artifacts/ver.txt
+else
+        version=0.0.1
+fi
+
+echo SEMOSS_VERSION environment variable is $SEMOSS_VERSION
+echo latest version is $latest_version
+echo current version is $version
+ 
+# If the version is being overridden, or the latest version is greater than the current, then update
+if ! [[ -z "${SEMOSS_VERSION}" ]] || [[ (( $latest_version > $version )) ]]; then
+        # Always use the overridden version if provided 
+        if [[ -z "${SEMOSS_VERSION}" ]]; then
+                version=$latest_version
+        else
+                version="${SEMOSS_VERSION}"
+        fi
+        
+        # Cleanup
+        rm -rf /opt/semoss-artifacts/artifacts/cluster/semoss*
+        rm -rf /root/.m2/repository/org/semoss
+       		
+        echo "Updating to version.. $version"
+        cd /opt/semoss-artifacts/artifacts/cluster && mvn clean install -Dci.version=$version
+        cp /opt/semoss-artifacts/artifacts/cluster/semosscluster-$version.jar /opt/semosscluster-$version.jar
+
+        echo "version=$latest_version" > /opt/semoss-artifacts/ver.txt
+        echo "updated=$last_updated" >> /opt/semoss-artifacts/ver.txt
+else
+        echo "Semoss is already up to date"
+fi
