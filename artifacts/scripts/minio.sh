c7959b06107a483b2707b9cc1848c95ce8578afe
tbanach <tbanach@deloitte.com>
2018-08-23 00:18:46 +0000

    bugfix : fixing minio functionality

--- artifacts/scripts/minio.sh
+++ artifacts/scripts/minio.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 minio gateway azure &
 mc config host add az http://127.0.0.1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
-mc cp --recursive az/${MINIO_DEFAULT_REMOTE}/* /opt/semosshome/
+rm -rf /opt/semosshome/*
+mc cp --recursive az/${MINIO_DEFAULT_REMOTE}/ /opt/semosshome
 mc mb az/${MINIO_REMOTE}
 mc mirror --watch --overwrite --remove /opt/semosshome az/${MINIO_REMOTE} &
