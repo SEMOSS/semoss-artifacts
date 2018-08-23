a1b10ecdde7038eaa10eaf1ef906512c8476de55
Banach <tbanach@deloitte.com>
2018-08-23 17:31:25 -0400

    update : sync script for minio

---
+++ artifacts/scripts/minio_sync.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Assume the MINIO_REMOTE, MINIO_ACCESS_KEY, and MINIO_SECRET_KEY 
+# environment variables are set
+
+# The directory to mirror
+directory=/opt/semosshome
+
+# Setup the gateway
+minio gateway azure &
+sleep 3
+
+# Add the host
+mc config host add az http://127.0.0.1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+sleep 3
+
+# If the remote exists, pull from it
+# Else, make the bucket and push to it
+if [[ "$(mc ls az)" = *"${MINIO_REMOTE}"* ]]; then
+	echo initial pull
+	mc mirror --overwrite az/${MINIO_REMOTE} ${directory} &
+else
+	echo creating remote
+	mc mb az/${MINIO_REMOTE}
+	echo initial push
+	mc mirror --overwrite --remove ${directory} az/${MINIO_REMOTE} &
+fi
+sleep 30
+
+# Sync
+# TODO JUST WAIT FOR PUSH AND PULL TO FINISH
+while [[ true ]] ; do
+	echo pull
+	mc mirror --overwrite az/${MINIO_REMOTE} ${directory} &
+	sleep 30
+	echo push
+	mc mirror --overwrite --remove ${directory} az/${MINIO_REMOTE} &
+	sleep 30
+done
