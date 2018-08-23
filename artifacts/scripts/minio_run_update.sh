f685b258d0f2104a7ba5426f815d162b64d8ce49
Banach <tbanach@deloitte.com>
2018-08-22 21:14:49 -0400

    bugfix : .sh ext

--- artifacts/scripts/minio_run_update.sh
+++ artifacts/scripts/minio_run_update.sh
@@ -1,9 +1,9 @@
-minio
 cd /opt/semoss-artifacts
 # Because chmod looks like a chage to git
 git checkout .
 git pull
 chmod 777 /opt/semoss-artifacts/artifacts/scripts/*
+minio.sh
 cd /opt/semoss-artifacts/artifacts/scripts
 ./update_latest_dev_no_lib.sh
 cd /opt/apache-tomcat-8.0.41/bin
