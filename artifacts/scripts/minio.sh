4a63d7d97d7e22a711990766a0a030addebfeadb
Banach <tbanach@deloitte.com>
2018-08-23 11:16:56 -0400

    update : syncing after update is complete

--- artifacts/scripts/minio.sh
+++ artifacts/scripts/minio.sh
@@ -5,5 +5,4 @@ mc config host add az http://127.0.0.1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_K
 rm -rf /opt/semosshome/*
 mc cp --recursive az/${MINIO_DEFAULT_REMOTE}/ /opt/semosshome
 mc mb az/${MINIO_REMOTE}
-mc mirror --watch --force --remove /opt/semosshome az/${MINIO_REMOTE} &
 sleep 3
