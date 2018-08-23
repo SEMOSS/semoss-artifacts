e387627c55ded525d9b2a94f60691fbed417fbb6
Banach <tbanach@deloitte.com>
2018-08-23 11:21:03 -0400

    revert : going back to previous config

--- artifacts/scripts/minio_run.sh
+++ artifacts/scripts/minio_run.sh
@@ -1,5 +1,3 @@
 minio.sh
-mc mirror --watch --force --remove /opt/semosshome az/${MINIO_REMOTE} &
-sleep 7
 cd /opt/apache-tomcat-8.0.41/bin
 ./catalina.sh run
--- artifacts/scripts/minio_run_update_all.sh
+++ artifacts/scripts/minio_run_update_all.sh
@@ -6,7 +6,5 @@ chmod 777 /opt/semoss-artifacts/artifacts/scripts/*
 minio.sh
 cd /opt/semoss-artifacts/artifacts/scripts
 ./update_latest_dev.sh
-mc mirror --watch --force --remove /opt/semosshome az/${MINIO_REMOTE} &
-sleep 7
 cd /opt/apache-tomcat-8.0.41/bin
 ./catalina.sh run
