906bd1cf6de14f20eac1934eaffae47cb786432e
Banach <tbanach@deloitte.com>
2018-08-23 12:30:44 -0400

    update : reverse sync trial

--- artifacts/scripts/minio.sh
+++ artifacts/scripts/minio.sh
@@ -5,6 +5,11 @@ mc config host add az http://127.0.0.1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_K
 rm -rf /opt/semosshome/*
 mc cp --recursive az/${MINIO_DEFAULT_REMOTE}/ /opt/semosshome
 mc mb az/${MINIO_REMOTE}
-mc mirror --watch --overwrite --remove /opt/semosshome az/${MINIO_REMOTE} &
-mc mirror --watch --overwrite --remove az/${MINIO_REMOTE} /opt/semosshome  &
+mc mirror --watch --overwrite /opt/semosshome az/${MINIO_REMOTE} &
+
+# For scheduled reverse syncs
+crontab -l > /opt/tempcron
+echo "* * * * * mc mirror az/${MINIO_REMOTE} /opt/semosshome &" >> /opt/tempcron
+crontab /opt/tempcron
+rm /opt/tempcron
 sleep 3
