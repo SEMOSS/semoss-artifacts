8d4e9b7ebcc47b269af08bd4a6a0849412dabc20
Banach <tbanach@deloitte.com>
2018-08-23 13:24:52 -0400

    update : removing crontab

--- artifacts/scripts/minio.sh
+++ artifacts/scripts/minio.sh
@@ -5,11 +5,5 @@ mc config host add az http://127.0.0.1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_K
 rm -rf /opt/semosshome/*
 mc cp --recursive az/${MINIO_DEFAULT_REMOTE}/ /opt/semosshome
 mc mb az/${MINIO_REMOTE}
-mc mirror --watch --overwrite /opt/semosshome az/${MINIO_REMOTE} &
-
-# For scheduled reverse syncs
-crontab -l > /opt/tempcron
-echo "* * * * * mc mirror az/${MINIO_REMOTE} /opt/semosshome &" >> /opt/tempcron
-crontab /opt/tempcron
-rm /opt/tempcron
+mc mirror --watch --overwrite --remove /opt/semosshome az/${MINIO_REMOTE} &
 sleep 3
