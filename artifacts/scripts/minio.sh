08c5a0af716d9f5019cc10fc65c4f3b8ccf91537
Banach <tbanach@deloitte.com>
2018-08-22 21:47:40 -0400

    bugfix : two sleeps

--- artifacts/scripts/minio.sh
+++ artifacts/scripts/minio.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 minio gateway azure &
-sleep 10
+sleep 7
 mc config host add az http://127.0.0.1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 rm -rf /opt/semosshome/*
 mc cp --recursive az/${MINIO_DEFAULT_REMOTE}/ /opt/semosshome
 mc mb az/${MINIO_REMOTE}
 mc mirror --watch --overwrite --remove /opt/semosshome az/${MINIO_REMOTE} &
+sleep 3
