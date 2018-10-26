98e5c458184498ad8ae517b6bcb9a9e4e5407bfd
kunal0137 <kunal0137@gmail.com>
2018-10-25 21:38:11 -0400

    Delete setSecurityOn.sh

--- artifacts/scripts/setSecurityOn.sh
+++
@@ -1,1 +0,0 @@
-sed -i '/<web-app.*/,/<\/web-app>/ {/<context-param>/,/<\/context-param>/ {/<param-name>security-enabled<\/param-name>/,/<param-value>/ s/false/true/}}' /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/web.xml
