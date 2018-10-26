b18e1b7096d6e8fb9909dd20f015c441143df4cf
kunal0137 <kunal0137@gmail.com>
2018-10-25 21:38:03 -0400

    Delete setRemoteSecurity.sh

--- artifacts/scripts/setRemoteSecurity.sh
+++
@@ -1,2 +0,0 @@
-sed -i "s/H2EmbeddedServerEngine/RDBMSNativeEngine/g" /opt/semosshome/db/security.smss
-sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SECURITY_IP\/database/g" /opt/semosshome/db/security.smss
