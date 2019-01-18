5a857de0320638a9e24bf5f08d94d0f677433f31
kunal0137 <kunal0137@gmail.com>
2019-01-18 15:32:45 -0500

    Update setAppConstants.sh

--- artifacts/scripts/setAppConstants.sh
+++ artifacts/scripts/setAppConstants.sh
@@ -1,1 +1,1 @@
-sed "s#Monolith#$MONOLITH_APPCONSTANT#g" /opt/apache-tomcat-8.0.41/webapps/SemossWeb/app.constants.js
+sed -i "s#Monolith#$MONOLITH_APPCONSTANT#g" /opt/apache-tomcat-8.0.41/webapps/SemossWeb/app.constants.js
