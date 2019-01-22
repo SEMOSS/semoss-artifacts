db0b19d0262eb4f3413bf59415bf9d311de1a492
kunal0137 <kunal0137@gmail.com>
2019-01-22 11:09:39 -0500

    Update runCS.sh

--- artifacts/scripts/runCS.sh
+++ artifacts/scripts/runCS.sh
@@ -17,4 +17,8 @@ if [ "$SET_APPCONSTANTS" = "true" ]
 then sh setAppConstants.sh
 fi
 cd /opt/apache-tomcat-8.0.41/bin
-./catalina.sh run
+./stop.sh
+cd /opt/semoss-artifacts/artifacts/scripts
+./update_latest_dev_no_lib.sh
+cd /opt/apache-tomcat-8.0.41/bin
+./start.sh
