c71aadaf81bf996678db6aba1185e2363e73c1e7
kunal0137 <kunal0137@gmail.com>
2019-01-22 10:53:44 -0500

    Update setAzureProps.sh

--- artifacts/scripts/setAzureProps.sh
+++ artifacts/scripts/setAzureProps.sh
@@ -1,2 +1,2 @@
-sed -i "s/<AZUREKEY>/$AZURE_ACCT_KEY/g" /opt/semosshome/RDF_Map.prop
-sed -i "s/<AZURENAME>/$AZURE_ACCT_NAME/g" /opt/semosshome/RDF_Map.prop
+sed -i "s@<AZUREKEY>@$AZURE_ACCT_KEY@g" /opt/semosshome/RDF_Map.prop
+sed -i "s@<AZURENAME>@$AZURE_ACCT_NAME@g" /opt/semosshome/RDF_Map.prop
