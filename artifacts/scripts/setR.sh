256d2a7bff53fabe4e0355f5c6b4d943696915ce
kunal0137 <kunal0137@gmail.com>
2019-08-06 13:45:45 -0400

    Create setR.sh

---
+++ artifacts/scripts/setR.sh
@@ -0,0 +1,23 @@
+if [ "$R_ON" = "true" ]
+sed -i "s@USE_R.*@USE_R\ttrue@g" /opt/semosshome/RDF_Map.prop
+fi
+
+if [ "$R_ON" = "false" ]
+sed -i "s@USE_R.*@USE_R\tfalse@g" /opt/semosshome/RDF_Map.prop
+fi
+
+if [ "$R_CONNECTION_TYPE" = "JRI" ]
+sed -i "s:R_CONNECTION_JRI.*:R_CONNECTION_JRI\ttrue:g" /opt/semosshome/RDF_Map.prop
+fi
+
+if [ "$R_CONNECTION_TYPE" = "Rserve" ]
+sed -i "s:R_CONNECTION_JRI.*:R_CONNECTION_JRI\tfalse:g" /opt/semosshome/RDF_Map.prop
+sed -i "s:IS_USER_RSERVE.*:IS_USER_RSERVE\tfalse:g" /opt/semosshome/RDF_Map.prop
+fi
+
+if [ "$R_CONNECTION_TYPE" = "User_Rserve_Pooled" ]
+sed -i "s:R_CONNECTION_JRI.*:R_CONNECTION_JRI\tfalse:g" /opt/semosshome/RDF_Map.prop
+sed -i "s:IS_USER_RSERVE.*:IS_USER_RSERVE\ttrue:g" /opt/semosshome/RDF_Map.prop
+sed -i "s:R_USER_CONNECTION_TYPE.*:R_USER_CONNECTION_TYPE\tpooled:g" /opt/semosshome/RDF_Map.prop
+sed -i "s:RSERVE_CONNECTION_POOL_SIZE.*:RSERVE_CONNECTION_POOL_SIZE\t$R_POOL_SIZE:g" /opt/semosshome/RDF_Map.prop
+fi
