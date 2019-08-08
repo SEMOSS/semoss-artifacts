a2e7c5f0d62fca9b090880548d8ec993f624b09f
kunal0137 <kunal0137@gmail.com>
2019-08-08 09:58:10 -0400

    Update runCS.sh

--- artifacts/scripts/runCS.sh
+++ artifacts/scripts/runCS.sh
@@ -30,13 +30,15 @@ if [ "$RSERVE_ON" = "true" ]
 then sh setRserveOn.sh
 fi
 if [[ -z "${MONOLITH_COOKIE}" ]]; then
-sh setMonolithCookie.sh
+echo "No Custom Monolith Cookie"
+else sh setMonolithCookie.sh
 fi
 if [ "$CUSTOM_MONOLITH_COOKIE" = "true" ]
 then sh setMonolithCookie.sh
 fi
 if [[ -z "${FILE_UPLOAD_LIMIT}" ]];
-then sh setFileUploadLimit.sh
+then echo "No updated file size limit" 
+else sh setFileUploadLimit.sh
 fi
 
 sh setR.sh
