0ca2e34c58de6fb76cead3a8eb2029f3db22e418
kunal0137 <kunal0137@gmail.com>
2019-08-21 15:14:53 -0400

    Create enableAnonymousUsers.sh

---
+++ artifacts/scripts/enableAnonymousUsers.sh
@@ -0,0 +1,1 @@
+sed '/<web-app.*/,/<\/web-app>/ {/<context-param>/,/<\/context-param>/ {/<param-name>anonymous-users-allowed<\/param-name>/,/<param-value>/ s/false/true/}}' /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/web.xml
