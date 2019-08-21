c7fe7b2745dfd06280924b5b708abd4030ae3578
kunal0137 <kunal0137@gmail.com>
2019-08-21 15:34:10 -0400

    Update enableAnonymousUserUpload.sh

--- artifacts/scripts/enableAnonymousUserUpload.sh
+++ artifacts/scripts/enableAnonymousUserUpload.sh
@@ -1,1 +1,1 @@
-sed '/<web-app.*/,/<\/web-app>/ {/<context-param>/,/<\/context-param>/ {/<param-name>anonymous-users-upload-data<\/param-name>/,/<param-value>/ s/false/true/}}' /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/web.xml
+sed -i '/<web-app.*/,/<\/web-app>/ {/<context-param>/,/<\/context-param>/ {/<param-name>anonymous-users-upload-data<\/param-name>/,/<param-value>/ s/false/true/}}' /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/web.xml
