fbed5018f272520534a7ffd44e7b4da5c992cbf6
kunal0137 <kunal0137@gmail.com>
2019-10-01 12:15:35 -0400

    update to tomcat_home fixed

--- artifacts/scripts/setMonolithCookie.sh
+++ artifacts/scripts/setMonolithCookie.sh
@@ -1,8 +1,8 @@
 if [[ -z "${TOMCAT_HOME}" ]]; then
-        if [ -d "$SCRIPT_TOMCAT_HOME" ] 
+        if [ -d "/opt/apache-tomcat-8.0.41" ] 
         then 
-                echo "Directory $SCRIPT_TOMCAT_HOME exists."
-                SCRIPT_TOMCAT_HOME=$SCRIPT_TOMCAT_HOME
+                echo "Directory /opt/apache-tomcat-8.0.41 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.0.41
                 echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
         elif [ -d "/opt/apache-tomcat-8.5.46" ] 
         then
