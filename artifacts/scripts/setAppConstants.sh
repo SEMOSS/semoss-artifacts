029e34db33ad5b150261b748cd580648fae2bd4d
kunal0137 <kunal0137@gmail.com>
2019-10-01 12:14:31 -0400

    update to tomcat_home

--- artifacts/scripts/setAppConstants.sh
+++ artifacts/scripts/setAppConstants.sh
@@ -1,1 +1,20 @@
-sed -i "s#Monolith#$MONOLITH_APPCONSTANT#g" /opt/apache-tomcat-8.0.41/webapps/SemossWeb/app.constants.js
+
+if [[ -z "${TOMCAT_HOME}" ]]; then
+        if [ -d "/opt/apache-tomcat-8.0.41" ] 
+        then 
+                echo "Directory /opt/apache-tomcat-8.0.41 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.0.41
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        elif [ -d "/opt/apache-tomcat-8.5.46" ] 
+        then
+                echo "Directory /opt/apache-tomcat-8.5.46 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.5.46
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        else 
+                echo "No Tomcat installation has been found"
+        fi
+else
+        echo "TOMCAT_HOME is predefined at $TOMCAT_HOME"
+        SCRIPT_TOMCAT_HOME="${TOMCAT_HOME}"
+fi
+sed -i "s#Monolith#$MONOLITH_APPCONSTANT#g" $SCRIPT_TOMCAT_HOME/webapps/SemossWeb/app.constants.js
