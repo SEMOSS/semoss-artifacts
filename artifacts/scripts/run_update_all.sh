c85e1cdab61e43b1651b2cf54816907bc60c1b74
kunal0137 <kunal0137@gmail.com>
2019-10-01 12:13:44 -0400

    update to tomcat_home

--- artifacts/scripts/run_update_all.sh
+++ artifacts/scripts/run_update_all.sh
@@ -1,3 +1,23 @@
+if [[ -z "${TOMCAT_HOME}" ]]; then
+        if [ -d "/opt/apache-tomcat-8.0.41" ] 
+        then 
+                echo "Directory /opt/apache-tomcat-8.0.41 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.0.41
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        elif [ -d "/opt/apache-tomcat-8.5.46" ] 
+        then
+                echo "Directory /opt/apache-tomcat-8.5.46 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.5.46
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        else 
+                echo "No Tomcat installation has been found"
+        fi
+else
+        echo "TOMCAT_HOME is predefined at $TOMCAT_HOME"
+        SCRIPT_TOMCAT_HOME="${TOMCAT_HOME}"
+fi
+
+
 cd /opt/semoss-artifacts
 # Because chmod looks like a chage to git
 git checkout .
@@ -5,5 +25,5 @@ git pull
 chmod 777 /opt/semoss-artifacts/artifacts/scripts/*
 cd /opt/semoss-artifacts/artifacts/scripts
 ./update_latest_dev.sh
-cd /opt/apache-tomcat-8.0.41/bin
+cd $SCRIPT_TOMCAT_HOME/bin
 ./catalina.sh run
