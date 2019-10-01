2ee5cdbe45184906fffb311840ec96e7e6a07ff2
kunal0137 <kunal0137@gmail.com>
2019-09-30 20:42:17 -0400

    Update update_latest_dev_snapshot.sh
    changing to $SCPRIT_TOMCAT_HOME

--- artifacts/scripts/update_latest_dev_snapshot.sh
+++ artifacts/scripts/update_latest_dev_snapshot.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [[ -z "${TOMCAT_HOME}" ]]; then
+	if [ -d "/opt/apache-tomcat-8.0.41" ] 
+	then 
+		echo "Directory /opt/apache-tomcat-8.0.41 exists."
+		SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.0.41
+	else if [ -d "/opt/apache-tomcat-8.5.46" ] 
+		echo "Directory /opt/apache-tomcat-8.5.46 exists."
+		SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.5.46
+	else 
+		echo "No Tomcat installation has been found"
+	fi
+else
+	SCRIPT_TOMCAT_HOME="${TOMCAT_HOME}"
+fi
+
+
 latest_version=`curl -s "https://oss.sonatype.org/content/repositories/snapshots/org/semoss/monolith/maven-metadata.xml" | grep "<latest>.*</latest>" | sed -e "s#\(.*\)\(<latest>\)\(.*\)\(</latest>\)\(.*\)#\3#g"`
 last_updated=`curl -s "https://oss.sonatype.org/content/repositories/snapshots/org/semoss/monolith/maven-metadata.xml" | grep "<lastUpdated>.*</lastUpdated>" | sed -e "s#\(.*\)\(<lastUpdated>\)\(.*\)\(</lastUpdated>\)\(.*\)#\3#g"`
 
@@ -31,29 +47,29 @@ if ! [[ -z "${SEMOSS_VERSION}" ]] || [[ (( $last_updated > $updated )) ]]; then
         rm -rf /root/.m2/repository/org/semoss
         cd /opt/semosshome
         find . -maxdepth 1 \! -name 'db' \! -name 'semoss-artifacts' \! -name '.' \! -name '..' -exec rm -rf {} +
-        rm -rf /opt/apache-tomcat-8.0.41/webapps/SemossWeb
-        rm -rf /opt/apache-tomcat-8.0.41/webapps/Monolith
+        rm -rf $SCRIPT_TOMCAT_HOME/webapps/SemossWeb
+        rm -rf $SCRIPT_TOMCAT_HOME/webapps/Monolith
 
         # Setup
-        mkdir -p /opt/apache-tomcat-8.0.41/webapps/SemossWeb
-        mkdir -p /opt/apache-tomcat-8.0.41/webapps/Monolith
+        mkdir -p $SCRIPT_TOMCAT_HOME/webapps/SemossWeb
+        mkdir -p $SCRIPT_TOMCAT_HOME/webapps/Monolith
 
         echo "Updating to version.. $version"
         cd /opt/semoss-artifacts/artifacts/home && mvn clean install -Dci.version=$version
         cp -r /opt/semoss-artifacts/artifacts/home/semoss*/* /opt/semosshome
         cd /opt/semoss-artifacts/artifacts/web && mvn clean install -Dci.version=$version
-        cp -r /opt/semoss-artifacts/artifacts/web/semoss*/* /opt/apache-tomcat-8.0.41/webapps/SemossWeb
+        cp -r /opt/semoss-artifacts/artifacts/web/semoss*/* $SCRIPT_TOMCAT_HOME/webapps/SemossWeb
         cd /opt/semoss-artifacts/artifacts/war && mvn clean install -Dci.version=$version
-        cp -r /opt/semoss-artifacts/artifacts/war/monolith*/* /opt/apache-tomcat-8.0.41/webapps/Monolith
+        cp -r /opt/semoss-artifacts/artifacts/war/monolith*/* $SCRIPT_TOMCAT_HOME/webapps/Monolith
         cd /opt/semoss-artifacts/artifacts/lib && mvn clean install -Dci.version=$version
-        cp -r /opt/semoss-artifacts/artifacts/lib/monolith*/* /opt/apache-tomcat-8.0.41/webapps/Monolith
+        cp -r /opt/semoss-artifacts/artifacts/lib/monolith*/* $SCRIPT_TOMCAT_HOME/webapps/Monolith
         cp -r /opt/semoss-artifacts/x/RDF_Map.prop /opt/semosshome
         cp -r /opt/semoss-artifacts/x/social.properties /opt/semosshome
         cp -r /opt/semoss-artifacts/x/log4j.prop /opt/semosshome 
-        cp -r /opt/semoss-artifacts/x/web.xml /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF 
+        cp -r /opt/semoss-artifacts/x/web.xml $SCRIPT_TOMCAT_HOME/webapps/Monolith/WEB-INF 
 
         # RDF bugfix
-        mv /opt/apache-tomcat-8.0.41/webapps/Monolith/WEB-INF/lib/dsiutils-2.4.2.jar /opt/apache-tomcat-8.0.41/lib
+        mv $SCRIPT_TOMCAT_HOME/webapps/Monolith/WEB-INF/lib/dsiutils-2.4.2.jar $SCRIPT_TOMCAT_HOME/lib
 
         echo "version=$latest_version" > /opt/semoss-artifacts/ver.txt
         echo "updated=$last_updated" >> /opt/semoss-artifacts/ver.txt
