a86ac7a5f4eaa38ef30436dfc3c934858d1a633e
kunal0137 <kunal0137@gmail.com>
2019-10-25 15:29:25 -0400

    Update setRemoteThemes.sh

--- artifacts/scripts/setRemoteThemes.sh
+++ artifacts/scripts/setRemoteThemes.sh
@@ -1,1 +1,1 @@
-sed "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_THEMES_IP\/database/g" /opt/semosshome/db/themes.smss
+sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_THEMES_IP\/database/g" /opt/semosshome/db/themes.smss
