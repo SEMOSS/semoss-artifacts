ed9d44d1e9f8bc07af1188f07755978ef7f2446f
kunal0137 <kunal0137@gmail.com>
2020-01-28 17:26:00 -0500

    Update setFileUploadLimit.sh
    /home/semoss

--- artifacts/scripts/setFileUploadLimit.sh
+++ artifacts/scripts/setFileUploadLimit.sh
@@ -1,1 +1,1 @@
-sed -i "s:FILE_TRANSFER_LIMIT.*:FILE_TRANSFER_LIMIT\t$FILE_UPLOAD_LIMIT:g" /opt/semosshome/RDF_Map.prop
+sed -i "s:FILE_TRANSFER_LIMIT.*:FILE_TRANSFER_LIMIT\t$FILE_UPLOAD_LIMIT:g" /home/semoss/semosshome/RDF_Map.prop
