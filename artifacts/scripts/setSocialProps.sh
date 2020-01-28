e70bb50fbd39edeb634363a78db517f8a9a49f67
kunal0137 <kunal0137@gmail.com>
2020-01-28 17:28:02 -0500

    Update setSocialProps.sh

--- artifacts/scripts/setSocialProps.sh
+++ artifacts/scripts/setSocialProps.sh
@@ -1,71 +1,71 @@
 if [ -n "$REDIRECT" ]
-then sed -i 's@<REDIRECT>@'"$REDIRECT"'@g' /opt/semosshome/social.properties 
+then sed -i 's@<REDIRECT>@'"$REDIRECT"'@g' $SEMOSS_BASE/semosshome/social.properties 
 fi
 
 if "$ENABLE_NATIVE" = "true"
-then sed -i "s/<NATIVE_ENABLE>/true/g" /opt/semosshome/social.properties
+then sed -i "s/<NATIVE_ENABLE>/true/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if "$ENABLE_NATIVE_REGISTRATION" = "true"
-then sed -i "s/<NATIVE_REGISTRATION_ENABLE>/true/g" /opt/semosshome/social.properties
+then sed -i "s/<NATIVE_REGISTRATION_ENABLE>/true/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if "$ENABLE_GOOGLE" = "true"
-then sed -i "s/<GOOGLE_ENABLE>/true/g" /opt/semosshome/social.properties
+then sed -i "s/<GOOGLE_ENABLE>/true/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if "$ENABLE_GITHUB" = "true"
-then sed -i "s/<GITHUB_ENABLE>/true/g" /opt/semosshome/social.properties
+then sed -i "s/<GITHUB_ENABLE>/true/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if "$ENABLE_DROPBOX" = "true"
-then sed -i "s/<DROPBOX_ENABLE>/true/g" /opt/semosshome/social.properties
+then sed -i "s/<DROPBOX_ENABLE>/true/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if "$ENABLE_MS" = "true"
-then sed -i "s/<MS_ENABLE>/true/g" /opt/semosshome/social.properties
+then sed -i "s/<MS_ENABLE>/true/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$GOOGLE_CLIENT_ID" ]
-then sed -i "s/<GOOGLECLIENTID>/$GOOGLE_CLIENT_ID/g" /opt/semosshome/social.properties
+then sed -i "s/<GOOGLECLIENTID>/$GOOGLE_CLIENT_ID/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$GOOGLE_SECRET_KEY" ]
-then sed -i "s/<GOOGLESECRETKEY>/$GOOGLE_SECRET_KEY/g" /opt/semosshome/social.properties
+then sed -i "s/<GOOGLESECRETKEY>/$GOOGLE_SECRET_KEY/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$GOOGLE_REDIRECT" ]
-then sed -i 's@<GOOGLEREDIRECT>@'"$GOOGLE_REDIRECT"'@g' /opt/semosshome/social.properties
+then sed -i 's@<GOOGLEREDIRECT>@'"$GOOGLE_REDIRECT"'@g' $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$GITHUB_CLIENT_ID" ]
-then sed -i "s/<GITHUBCLIENTID>/$GITHUB_CLIENT_ID/g" /opt/semosshome/social.properties
+then sed -i "s/<GITHUBCLIENTID>/$GITHUB_CLIENT_ID/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$GITHUB_SECRET_KEY" ]
-then sed -i "s/<GITHUBSECRETKEY>/$GITHUB_SECRET_KEY/g" /opt/semosshome/social.properties
+then sed -i "s/<GITHUBSECRETKEY>/$GITHUB_SECRET_KEY/g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$GITHUB_REDIRECT" ]
-then sed -i 's@<GITHUBREDIRECT>@'"$GITHUB_REDIRECT"'@g' /opt/semosshome/social.properties
+then sed -i 's@<GITHUBREDIRECT>@'"$GITHUB_REDIRECT"'@g' $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$MS_AUTHORITY" ]
-then sed -i "s@<MSAUTHORITY>@$MS_AUTHORITY@g" /opt/semosshome/social.properties
+then sed -i "s@<MSAUTHORITY>@$MS_AUTHORITY@g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$MS_TENANT" ]
-then sed -i "s@<MSTENANT>@$MS_TENANT@g" /opt/semosshome/social.properties
+then sed -i "s@<MSTENANT>@$MS_TENANT@g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$MS_CLIENT_ID" ]
-then sed -i "s@<MSCLIENTID>@$MS_CLIENT_ID@g" /opt/semosshome/social.properties
+then sed -i "s@<MSCLIENTID>@$MS_CLIENT_ID@g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$MS_SECRET_KEY" ]
-then sed -i "s#<MSSECRETKEY>#$MS_SECRET_KEY#g" /opt/semosshome/social.properties
+then sed -i "s#<MSSECRETKEY>#$MS_SECRET_KEY#g" $SEMOSS_BASE/semosshome/social.properties
 fi
 
 if [ -n "$MS_REDIRECT" ]
-then sed -i "s@<MSREDIRECT>@$MS_REDIRECT@g" /opt/semosshome/social.properties
+then sed -i "s@<MSREDIRECT>@$MS_REDIRECT@g" $SEMOSS_BASE/semosshome/social.properties
 fi
