1628efb819f10c8c5c0d2f644b9f05596654bbef
kunal0137 <kunal0137@gmail.com>
2020-03-16 16:18:04 -0400

    Update setRemoteSecurity.sh

--- artifacts/scripts/setRemoteSecurity.sh
+++ artifacts/scripts/setRemoteSecurity.sh
@@ -1,2 +1,2 @@
 sed -i "s/H2EmbeddedServerEngine/RDBMSNativeEngine/g" /opt/semosshome/db/security.smss
-sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SECURITY_IP\/database/g" /opt/semosshome/db/security.smss
+sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$SECURITY_SERVICE_HOST:$SECURITY_SERVICE_PORT\/database/g" /opt/semosshome/db/security.smss
