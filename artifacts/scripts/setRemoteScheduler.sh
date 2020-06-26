f5fa341dd297500b531334d13a8a5c6a043441bf
kunal0137 <kunal0137@gmail.com>
2020-06-26 14:30:56 -0400

    Update setRemoteScheduler.sh

--- artifacts/scripts/setRemoteScheduler.sh
+++ artifacts/scripts/setRemoteScheduler.sh
@@ -1,2 +1,2 @@
 sed -i "s/H2EmbeddedServerEngine/RDBMSNativeEngine/g" /opt/semosshome/db/scheduler.smss
-sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SECURITY_IP\/database/g" /opt/semosshome/db/scheduler.smss
+sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SCHEDULER_IP\/database/g" /opt/semosshome/db/scheduler.smss
