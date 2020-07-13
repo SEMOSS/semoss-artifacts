b8de59011ccd02a36253abd51013ad86e9f42a6f
kunal0137 <kunal0137@gmail.com>
2020-07-13 10:25:57 -0500

    Update setRemoteScheduler.sh
    setting db close on exit to be false

--- artifacts/scripts/setRemoteScheduler.sh
+++ artifacts/scripts/setRemoteScheduler.sh
@@ -1,2 +1,2 @@
 sed -i "s/H2EmbeddedServerEngine/RDBMSNativeEngine/g" /opt/semosshome/db/scheduler.smss
-sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SCHEDULER_IP\/database/g" /opt/semosshome/db/scheduler.smss
+sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SCHEDULER_IP\/database;DB_CLOSE_ON_EXIT=FALSE;/g" /opt/semosshome/db/scheduler.smss
