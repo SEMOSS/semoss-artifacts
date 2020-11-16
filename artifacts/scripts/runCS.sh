b1db4442db55058fee031088086698671c3967c7
kunal0137 <kunal0137@gmail.com>
2020-11-16 11:26:47 -0500

    Update runCS.sh

--- artifacts/scripts/runCS.sh
+++ artifacts/scripts/runCS.sh
@@ -91,6 +91,10 @@ if [[ -z "${SCHEDULER_ENDPOINT}" ]];
 then echo "No custom scheduler url defined" 
 else sh setSchedulerUrl.sh
 fi
+if [[ -z "${PM_SEMOSS_EXECUTE_SQL_ENCRYPTION_PASSWORD}" ]];
+then echo "No SQL Encryption Password" 
+else sh setSQLEncryptionPassword.sh
+fi
 sh setR.sh
 sh setPy.sh
 cd $SCRIPT_TOMCAT_HOME/bin
