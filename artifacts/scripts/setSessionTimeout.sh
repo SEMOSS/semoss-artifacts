0f61f1bbd91a9f9f6082c1f9787010f2ba1674b7
kunal0137 <kunal0137@gmail.com>
2020-11-17 18:25:39 -0500

    Create setSessionTimeout.sh

---
+++ artifacts/scripts/setSessionTimeout.sh
@@ -0,0 +1,22 @@
+
+
+if [ -z "${TOMCAT_HOME}" ]; then
+        if [ -d "/opt/apache-tomcat-8.0.41" ] 
+        then 
+                echo "Directory /opt/apache-tomcat-8.0.41 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.0.41
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        elif [ -d "/opt/apache-tomcat-9.0.26" ] 
+        then
+                echo "Directory /opt/apache-tomcat-9.0.26 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-9.0.26
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        else 
+                echo "No Tomcat installation has been found"
+        fi
+else
+        echo "TOMCAT_HOME is predefined at $TOMCAT_HOME"
+        SCRIPT_TOMCAT_HOME="${TOMCAT_HOME}"
+fi
+
+sed -i "s#<session-timeout>.*#<session-timeout>$SESSION_TIMEOUT</session-timeout>#g"  $SCRIPT_TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
