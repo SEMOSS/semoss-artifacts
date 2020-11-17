1a7afd8b47e0c5800fbbe954679974f9ce417350
kunal0137 <kunal0137@gmail.com>
2020-11-17 17:24:39 -0500

    Create setTrustedTokenDomain.sh

---
+++ artifacts/scripts/setTrustedTokenDomain.sh
@@ -0,0 +1,19 @@
+if [ -z "${TOMCAT_HOME}" ]; then
+        if [ -d "/opt/apache-tomcat-8.0.41" ] 
+        then 
+                echo "Directory /opt/apache-tomcat-8.0.41 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-8.0.41
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        elif [ -d "/opt/apache-tomcat-9.0.26" ] 
+        then
+                echo "Directory /opt/apache-tomcat-9.0.26 exists."
+                SCRIPT_TOMCAT_HOME=/opt/apache-tomcat-9.0.26
+                echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        else 
+                echo "No Tomcat installation has been found"
+        fi
+else
+        echo "TOMCAT_HOME is predefined at $TOMCAT_HOME"
+        SCRIPT_TOMCAT_HOME="${TOMCAT_HOME}"
+
+sed "/<web-app.*/,/<\/web-app>/ {/<filter>/,/<\/filter>/ {/<init-param>/,/<\/init-param/ {/<param-name>trustedTokenDomain<\/param-name>/,/<param-value>/ s/*/$TRUSTED_TOKEN_DOMAIN/}}}" $SCRIPT_TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
