176b7f49601638320e0c62fb319e44e92fb696fc
themaherkhalil <khalil.maher91@gmail.com>
2020-11-21 13:42:26 -0500

    Update setRemoteThemes.sh

--- artifacts/scripts/setRemoteThemes.sh
+++ artifacts/scripts/setRemoteThemes.sh
@@ -1,1 +1,10 @@
-sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_THEMES_IP\/database/g" /opt/semosshome/db/themes.smss
+sed -i "s/H2EmbeddedServerEngine/RDBMSNativeEngine/g" /opt/semosshome/db/themes.smss
+
+if [[ -z "${REMOTE_THEMES_IP}" ]];
+then
+  echo "Remote Themes IP not defined. Using SERVICE_HOST and PORT"
+  sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$THEMES_SERVICE_HOST:$THEMES_SERVICE_PORT\/database/g" /opt/semosshome/db/database.smss
+else
+  echo "Remote Themes IP defined"
+  sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_THEMES_IP\/database/g" /opt/semosshome/db/themes.smss
+fi
