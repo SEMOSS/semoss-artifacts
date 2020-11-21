4661f21e2c8c8575ca091e53d8b658bb7f3d9e41
themaherkhalil <khalil.maher91@gmail.com>
2020-11-21 13:37:25 -0500

    Update setRemoteSecurity.sh

--- artifacts/scripts/setRemoteSecurity.sh
+++ artifacts/scripts/setRemoteSecurity.sh
@@ -1,2 +1,10 @@
 sed -i "s/H2EmbeddedServerEngine/RDBMSNativeEngine/g" /opt/semosshome/db/security.smss
-sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SECURITY_IP\/database/g" /opt/semosshome/db/security.smss
+
+if [[ -z "${REMOTE_SECURITY_IP}" ]];
+then
+  echo "Remote Security IP not defined. Using SERVICE_HOST and PORT"
+  sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$SECURITY_SERVICE_HOST:$SECURITY_SERVICE_PORT\/database/g" /opt/semosshome/db/security.smss
+else
+  echo "Remote Security IP defined"
+  sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SECURITY_IP\/database/g" /opt/semosshome/db/security.smss
+fi
