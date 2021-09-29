1f462109d1d8f8bba2607af3877e3e5d1681b39e
kunal0137 <kunal0137@gmail.com>
2021-09-29 15:30:59 -0400

    Update setNettyPython.sh

--- artifacts/scripts/setNettyPython.sh
+++ artifacts/scripts/setNettyPython.sh
@@ -1,1 +1,9 @@
 sed -i "s@NETTY_PYTHON.*@NETTY_PYTHON\t$NETTY_PYTHON@g" /opt/semosshome/RDF_Map.prop
+if [[ -z "${TCP_WORKER}" ]];
+then echo "No TCP_WORKER defined" 
+else sed -i "s@TCP_WORKER.*@TCP_WORKER\t$TCP_WORKER@g" /opt/semosshome/RDF_Map.prop
+fi
+if [[ -z "${TCP_CLIENT}" ]];
+then echo "No TCP_CLIENT defined" 
+else sed -i "s@TCP_CLIENT.*@TCP_CLIENT\t$TCP_CLIENT@g" /opt/semosshome/RDF_Map.prop
+fi
