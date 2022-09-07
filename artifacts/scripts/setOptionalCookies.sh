21506eb3d91f0a3b389f379015b2f97c7c8414d6
kunal0137 <kunal0137@gmail.com>
2022-09-07 09:46:05 -0400

    Create setOptionalCookies.sh

---
+++ artifacts/scripts/setOptionalCookies.sh
@@ -0,0 +1,16 @@
+if [ -z "${TOMCAT_HOME}" ]; then
+	SCRIPT_TOMCAT_HOME="$(ls /opt/ | grep "apache-tomcat")"
+	echo "TOMCAT_HOME is $SCRIPT_TOMCAT_HOME"
+        if [ -z "${SCRIPT_TOMCAT_HOME}" ]; then
+                exit 1
+        fi
+else
+	echo "TOMCAT_HOME is predefined at $TOMCAT_HOME"
+	SCRIPT_TOMCAT_HOME="${TOMCAT_HOME}"
+fi
+
+if [[ -z $OPTIONAL_COOKIES ]]; then
+echo "No change in optional cookies"
+else sed -i "s@.constant('OPTIONAL_COOKIES'.*@.constant('OPTIONAL_COOKIES',$OPTIONAL_COOKIES)@g" $SCRIPT_TOMCAT_HOME/webapps/SemossWeb/app.constants.js
+fi
+
