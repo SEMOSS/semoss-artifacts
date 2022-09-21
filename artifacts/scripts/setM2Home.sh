fb08f6e218092bc7270462d5c9657ed6f7fbc7d2
kunal0137 <kunal0137@gmail.com>
2022-09-21 16:48:05 -0400

    Update setM2Home.sh

--- artifacts/scripts/setM2Home.sh
+++ artifacts/scripts/setM2Home.sh
@@ -10,4 +10,4 @@ else
 fi
 
 sed  -i "s@<localRepository>/path/to/local/repo</localRepository>@--><localRepository>$CUSTOM_M2</localRepository><!--@g" $SCRIPT_M2_HOME/conf/settings.xml
-sed -i "1s/^/REPO_HOME\t$CUSTOM_M2\n/" /opt/semosshome/RDF_Map.prop
+sed -i '1s/^/REPO_HOME\t'$CUSTOM_M2'\n/' /opt/semosshome/RDF_Map.prop
