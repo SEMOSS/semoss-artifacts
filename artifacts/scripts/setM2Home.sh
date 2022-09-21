508547c25f7cd2e40e2eb0404471c50e19d8b06d
kunal0137 <kunal0137@gmail.com>
2022-09-21 16:15:23 -0400

    Update setM2Home.sh

--- artifacts/scripts/setM2Home.sh
+++ artifacts/scripts/setM2Home.sh
@@ -1,5 +1,5 @@
 if [ -z "${M2_HOME}" ]; then
-	SCRIPT_M2_HOME=/opt"$(ls /opt/ | grep "apache-maven")"
+	SCRIPT_M2_HOME=/opt/"$(ls /opt/ | grep "apache-maven")"
 	echo "M2_HOME is $SCRIPT_M2_HOME"
         if [ -z "${SCRIPT_M2_HOME}" ]; then
                 exit 1
