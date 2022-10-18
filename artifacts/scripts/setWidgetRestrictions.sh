622d0093195548acf352ec5fab98ffe36fc84b47
Maher Khalil <themaherkhalil@gmail.com>
2022-10-18 12:24:57 -0400

    Adding share/export widget list parameterization

--- artifacts/scripts/setWidgetRestrictions.sh
+++ artifacts/scripts/setWidgetRestrictions.sh
@@ -7,3 +7,8 @@ if [[ -z $PIPELINE_SOURCE_FILTER ]]; then
 echo "No pipeline source filter"
 else sed -i "s@PIPELINE_SOURCE_FILTER.*@PIPELINE_SOURCE_FILTER\t$PIPELINE_SOURCE_FILTER@g" /opt/semosshome/RDF_Map.prop
 fi
+
+if [[ -z $WIDGET_TAB_SHARE_EXPORT_LIST ]]; then
+echo "No widget tab share/export filtering"
+else sed -i "s@WIDGET_TAB_SHARE_EXPORT_LIST.*@WIDGET_TAB_SHARE_EXPORT_LIST\t$WIDGET_TAB_SHARE_EXPORT_LIST@g" /opt/semosshome/RDF_Map.prop
+fi
