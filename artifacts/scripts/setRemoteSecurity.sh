cc0d4bc4bcdedc11b27cbf3d34f370c024ad03dc
kunal0137 <kunal0137@gmail.com>
2022-11-02 20:40:04 -0400

    Update setRemoteSecurity.sh

--- artifacts/scripts/setRemoteSecurity.sh
+++ artifacts/scripts/setRemoteSecurity.sh
@@ -5,6 +5,6 @@ then
   echo "Remote Security IP not defined. Using SERVICE_HOST and PORT"
   sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$SECURITY_SERVICE_HOST:$SECURITY_SERVICE_PORT\/database/g" /opt/semosshome/db/security.smss
 else
-  echo "Remote Security IP defined"
+  echo "Remote Security IP defined: it is $REMOTE_SECURITY_IP ip"
   sed -i "s/jdbc:h2:nio:@BaseFolder@\/db\/@ENGINE@\/database/jdbc:h2:tcp:\/\/$REMOTE_SECURITY_IP\/database/g" /opt/semosshome/db/security.smss
 fi
