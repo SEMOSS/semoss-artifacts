4253956e0fa66d7ff97456aa56f49fbc82cae421
themaherkhalil <khalil.maher91@gmail.com>
2023-03-22 15:34:27 -0400

    Update setSocialProps.sh

--- artifacts/scripts/setSocialProps.sh
+++ artifacts/scripts/setSocialProps.sh
@@ -190,6 +190,13 @@ if [ -n "$LINOTP_CHECK_AD" ]
 then sed -i "s@linotp_check_ad.*@linotp_check_ad\t$LINOTP_CHECK_AD@g" /opt/semosshome/social.properties
 fi
 
+if [ -n "$LINOTP_ADMIN_USER" ]
+then sed -i "s@linotp_adminuser.*@linotp_adminuser\t$LINOTP_ADMIN_USER@g" /opt/semosshome/social.properties
+fi
+
+if [ -n "$LINOTP_ADMIN_PASSWORD" ]
+then sed -i "s@linotp_adminpassword.*@linotp_adminpassword\t$LINOTP_ADMIN_PASSWORD@g" /opt/semosshome/social.properties
+fi
 
 
 
