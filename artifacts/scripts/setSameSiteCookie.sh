9bff59649c5510e4ce31a391f4b5a3fdc272b3f8
themaherkhalil <khalil.maher91@gmail.com>
2023-05-17 11:30:37 -0400

    Update setSameSiteCookie.sh

--- artifacts/scripts/setSameSiteCookie.sh
+++ artifacts/scripts/setSameSiteCookie.sh
@@ -1,1 +1,2 @@
 sed -i "s@SAMESITE_COOKIE.*@SAMESITE_COOKIE\t$SAMESITE_COOKIE@g" /opt/semosshome/RDF_Map.prop
+sed -i "s@sameSiteCookies=\".*\"@sameSiteCookies=\"$SAMESITE_COOKIE\"@g" $TOMCAT_HOME/conf/context.xml
