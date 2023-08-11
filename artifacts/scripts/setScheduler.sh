69da4ab40e150f234290e3f3a488f5c90d4bbbd5
Khalil <mahkhalil@deloitte.com>
2023-08-11 17:11:32 -0400

    cleanup for how scheduler endpoint/properties are set

--- artifacts/scripts/setScheduler.sh
+++ artifacts/scripts/setScheduler.sh
@@ -1,1 +1,27 @@
-sed -i 's@<SCHEDULER_FORCE_DISABLE>@true@g' /opt/semosshome/RDF_Map.prop
+
+if [[ -z "${SCHEDULER_FORCE_DISABLE}" ]]; then 
+     echo "No custom scheduler force disable"
+else 
+    sed -i "s@SCHEDULER_FORCE_DISABLE.*@SCHEDULER_FORCE_DISABLE\t$SCHEDULER_FORCE_DISABLE@g" /opt/semosshome/RDF_Map.prop
+fi
+if [[ -z "${SCHEDULER_ENDPOINT}" ]]; then 
+    echo "No custom scheduler url defined" 
+else 
+    sed -i "s@SCHEDULER_ENDPOINT.*@SCHEDULER_ENDPOINT\t$SCHEDULER_ENDPOINT@g" /opt/semosshome/RDF_Map.prop
+fi
+if [[ -z "${SCHEDULER_KEYSTORE}" ]]; then 
+     echo "No custom scheduler keystore"
+else 
+    sed -i "s@SCHEDULER_KEYSTORE.*@SCHEDULER_KEYSTORE\t$SCHEDULER_KEYSTORE@g" /opt/semosshome/RDF_Map.prop
+fi
+if [[ -z "${SCHEDULER_KEYSTORE_PASSWORD}" ]]; then 
+     echo "No custom scheduler keystore password"
+else 
+    sed -i "s@SCHEDULER_KEYSTORE_PASSWORD.*@SCHEDULER_KEYSTORE_PASSWORD\t$SCHEDULER_KEYSTORE_PASSWORD@g" /opt/semosshome/RDF_Map.prop
+fi
+if [[ -z "${SCHEDULER_CERTIFICATE_PASSWORD}" ]]; then 
+     echo "No custom scheduler cert password"
+else 
+    sed -i "s@SCHEDULER_CERTIFICATE_PASSWORD.*@SCHEDULER_CERTIFICATE_PASSWORD\t$SCHEDULER_CERTIFICATE_PASSWORD@g" /opt/semosshome/RDF_Map.prop
+fi
+
--- artifacts/scripts/setSchedulerUrl.sh
+++
@@ -1,1 +0,0 @@
-sed -i "s@SCHEDULER_ENDPOINT.*@SCHEDULER_ENDPOINT\t$SCHEDULER_ENDPOINT@g" /opt/semosshome/RDF_Map.prop
