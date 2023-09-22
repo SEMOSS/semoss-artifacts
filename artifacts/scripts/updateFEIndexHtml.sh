aea3640cc4644f1505ef07f4880cfde8a803de78
kunal0137 <kunal0137@gmail.com>
2023-09-22 16:24:34 -0400

    Update updateFEIndexHtml.sh

--- artifacts/scripts/updateFEIndexHtml.sh
+++ artifacts/scripts/updateFEIndexHtml.sh
@@ -1,6 +1,8 @@
 if [[ -z "${FE_ROUTE}" ]]; then 
     echo "No custom route exist" 
     sed -i 's@<!doctype html><html lang="en" ng-app="app"><head>@<!doctype html><html lang="en" ng-app="app"><head><script id="semoss-env" type="application/json">{"MODULE":"Monolith"}</script>@g' $TOMCAT_HOME/webapps/SemossWeb/index.html
+    sed -i 's@<!doctype html><html lang="en" ng-app="app"><head>@<!doctype html><html lang="en" ng-app="app"><head><script id="semoss-env" type="application/json">{"MODULE":"Monolith"}</script>@g' $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
 else 
     sed -i "s@<!doctype html><html lang=\"en\" ng-app=\"app\"><head>@<!doctype html><html lang=\"en\" ng-app=\"app\"><head><script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>@g" $TOMCAT_HOME/webapps/SemossWeb/index.html
+    sed -i "s@<!doctype html><html lang=\"en\" ng-app=\"app\"><head>@<!doctype html><html lang=\"en\" ng-app=\"app\"><head><script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>@g" $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
 fi
