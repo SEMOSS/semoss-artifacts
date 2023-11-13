82973ee29d9c197995792c9440454c85dfc5127f
kunal0137 <kunal0137@gmail.com>
2023-11-13 10:00:06 -0600

    Update updateFEIndexHtml.sh

--- artifacts/scripts/updateFEIndexHtml.sh
+++ artifacts/scripts/updateFEIndexHtml.sh
@@ -5,5 +5,4 @@ if [[ -z "${FE_ROUTE}" ]]; then
 else 
     #sed -i "s@<!doctype html><html lang=\"en\" ng-app=\"app\"><head>@<!doctype html><html lang=\"en\" ng-app=\"app\"><head><script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>@g" $TOMCAT_HOME/webapps/SemossWeb/index.html
     #sed -i "s@<!doctype html><html><head><meta charset=\"utf-8\"/>@<!doctype html><html><head><meta charset=\"utf-8\"/><script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>@g" $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
-    sed -i "/<head>/a\        <script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>" index.html
-fi
+    sed -i "/<head>/a\        <script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>"  $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
