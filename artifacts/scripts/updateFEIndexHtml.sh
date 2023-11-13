0be8357d4ae00010a60976ae50413ab528e1638f
kunal0137 <kunal0137@gmail.com>
2023-11-13 11:53:38 -0600

    Update updateFEIndexHtml.sh

--- artifacts/scripts/updateFEIndexHtml.sh
+++ artifacts/scripts/updateFEIndexHtml.sh
@@ -3,6 +3,6 @@ if [[ -z "${FE_ROUTE}" ]]; then
     sed -i 's@<!doctype html><html lang="en" ng-app="app"><head>@<!doctype html><html lang="en" ng-app="app"><head><script id="semoss-env" type="application/json">{"MODULE":"Monolith"}</script>@g' $TOMCAT_HOME/webapps/SemossWeb/index.html
     sed -i 's@<!doctype html><html><head><meta charset=\"utf-8\"/>@<!doctype html><html><head><meta charset=\"utf-8\"/><script id="semoss-env" type="application/json">{"MODULE":"Monolith"}</script>@g' $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
 else 
+    sed -i "/<head>/a\        <script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>"  $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
     #sed -i "s@<!doctype html><html lang=\"en\" ng-app=\"app\"><head>@<!doctype html><html lang=\"en\" ng-app=\"app\"><head><script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>@g" $TOMCAT_HOME/webapps/SemossWeb/index.html
     #sed -i "s@<!doctype html><html><head><meta charset=\"utf-8\"/>@<!doctype html><html><head><meta charset=\"utf-8\"/><script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>@g" $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
-    sed -i "/<head>/a\        <script id=\"semoss-env\" type=\"application/json\">{\"MODULE\":\"$FE_ROUTE\"}</script>"  $TOMCAT_HOME/webapps/SemossWeb/packages/client/dist/index.html
