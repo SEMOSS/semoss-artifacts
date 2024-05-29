1fe09818da519776ad649848c4ed0ad5d1421a1e
Maher Khalil <themaherkhalil@gmail.com>
2024-05-29 11:55:49 -0400

    changing to whole line replacement

--- artifacts/scripts/setWhiteListDomains.sh
+++ artifacts/scripts/setWhiteListDomains.sh
@@ -1,3 +1,3 @@
 if [ -n "$WHITE_LIST_DOMAINS" ]
-then sed -i 's@<WHITE_LIST_DOMAINS>@'"$WHITE_LIST_DOMAINS"'@g' opt/semosshome/RDF_Map.prop
+then sed -i "s@WHITE_LIST_DOMAINS.*@WHITE_LIST_DOMAINS\t$WHITE_LIST_DOMAINS@g" opt/semosshome/RDF_Map.prop
 fi
