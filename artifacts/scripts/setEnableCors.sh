3bc8229d97c306dafb732fcfa3cba8d01287c880
kunal0137 <kunal0137@gmail.com>
2025-03-28 10:41:50 -0400

    Update setEnableCors.sh

--- artifacts/scripts/setEnableCors.sh
+++ artifacts/scripts/setEnableCors.sh
@@ -1,3 +1,5 @@
+### DEPRECATED THROUGH setCors.sh
+
 #!/bin/bash
 sed -i '/<!--CORS_FILTER_FLAG/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
 sed -i '/CORS_FILTER_FLAG_END-->/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
