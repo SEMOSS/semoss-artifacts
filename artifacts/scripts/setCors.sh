36598633dbb10113ea75292401dda768d3e819b8
kunal0137 <kunal0137@gmail.com>
2025-04-15 15:14:38 -0400

    adding checks to setCors

--- artifacts/scripts/setCors.sh
+++ artifacts/scripts/setCors.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-#enable cors - activate using $ENABLE_CORS true
+# Enable CORS - activate using $ENABLE_CORS true
 sed -i '/<!--CORS_FILTER_FLAG/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
 sed -i '/CORS_FILTER_FLAG_END-->/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
 
-#setting cors allowed origin
-sed -i '/<web-app.*/,/<\/web-app>/ {/<filter>/,/<\/filter>/ {/<init-param>/,/<\/init-param/ {/<param-name>cors.allowed.origins<\/param-name>/,/<param-value>/  s/*/CONFIG_SET_CUSTOM_CORS_ORIGINS/}}}' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
-sed -i "s|CONFIG_SET_CUSTOM_CORS_ORIGINS|$CORS_ALLOWED_ORIGINS|g" $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+# Check if CORS_ALLOWED_ORIGINS is defined
+if [ -n "$CORS_ALLOWED_ORIGINS" ]; then
+    # Setting CORS allowed origin
+    sed -i '/<web-app.*/,/<\/web-app>/ {/<filter>/,/<\/filter>/ {/<init-param>/,/<\/init-param/ {/<param-name>cors.allowed.origins<\/param-name>/,/<param-value>/  s/*/CONFIG_SET_CUSTOM_CORS_ORIGINS/}}}' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+    sed -i "s|CONFIG_SET_CUSTOM_CORS_ORIGINS|$CORS_ALLOWED_ORIGINS|g" $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+fi
 
-#setting support credentials
-sed -i '/<web-app.*/,/<\/web-app>/ {/<filter>/,/<\/filter>/ {/<init-param>/,/<\/init-param/ {/<param-name>cors.support.credentials<\/param-name>/,/<param-value>/  s/false/CONFIG_SET_CUSTOM_CORS_SUPPORT_CREDENTIALS/}}}' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
-sed -i "s|CONFIG_SET_CUSTOM_CORS_SUPPORT_CREDENTIALS|$CORS_SUPPORT_CREDENTIALSS|g" $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+# Check if CORS_SUPPORT_CREDENTIALS is defined
+if [ -n "$CORS_SUPPORT_CREDENTIALS" ]; then
+    # Setting support credentials
+    sed -i '/<web-app.*/,/<\/web-app>/ {/<filter>/,/<\/filter>/ {/<init-param>/,/<\/init-param/ {/<param-name>cors.support.credentials<\/param-name>/,/<param-value>/  s/false/CONFIG_SET_CUSTOM_CORS_SUPPORT_CREDENTIALS/}}}' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+    sed -i "s|CONFIG_SET_CUSTOM_CORS_SUPPORT_CREDENTIALS|$CORS_SUPPORT_CREDENTIALS|g" $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+fi
