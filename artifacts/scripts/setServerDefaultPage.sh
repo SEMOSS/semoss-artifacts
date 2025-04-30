a8dbfd593492edd4ab81186665f285b651bfe5fe
Maher Khalil <themaherkhalil@gmail.com>
2025-04-30 12:35:00 +0530

    feat: exposing ability to set the server.xml docBase and Monolith WEB-Content/login/index.html url

---
+++ artifacts/scripts/setServerDefaultPage.sh
@@ -0,0 +1,11 @@
+# if you want to direct directly to our auth endpoints
+# you will set CUSTOM_DEFAULT_PAGE to Monolith/login
+# and you will then set the CUSTOM_DEFAULT_PAGE_URL to https://YOUR_DNS/Monolith/api/auth/<your provider>
+
+if [ -n "$CUSTOM_DEFAULT_PAGE" ]; then
+    sed -i 's|docBase="SemossWeb"|docBase='"\"$CUSTOM_DEFAULT_PAGE"\"'|g' $TOMCAT_HOME/conf/server.xml
+fi
+
+if [ -n "$CUSTOM_DEFAULT_PAGE_URL" ]; then
+    sed -i 's|http://www.google.com|$CUSTOM_DEFAULT_PAGE_URL|g' $TOMCAT_HOME/webapps/Monolith/login/index.html
+fi
