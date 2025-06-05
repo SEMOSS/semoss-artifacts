e391589b09e919d157389e9bd952acec5894c82d
kunal0137 <kunal0137@gmail.com>
2025-06-05 12:56:32 -0400

    Update setMonolithSecureCookie.sh

--- artifacts/scripts/setMonolithSecureCookie.sh
+++ artifacts/scripts/setMonolithSecureCookie.sh
@@ -1,3 +1,3 @@
 # old - commenting out for now such that we can 
 #sed -i "s#<secure>true</secure>#<secure>$MONOLITH_COOKIE_SET_SECURE</secure>#g" $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
-sed -i "s#<secure>.*</secure>#<secure>MONOLITH_COOKIE_SET_SECURE</secure>#g" "$TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml"
+sed -i "s#<secure>.*</secure>#<secure>$MONOLITH_COOKIE_SET_SECURE</secure>#g" $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
