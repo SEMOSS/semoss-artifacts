f0cf93ef1e0a11612a8703fa9eb95420d6bec118
kunal0137 <kunal0137@gmail.com>
2026-02-12 12:32:47 -0500

    adding shebang for playwright export

--- artifacts/scripts/setPlaywrightExport.sh
+++ artifacts/scripts/setPlaywrightExport.sh
@@ -1,1 +1,2 @@
- sed -i "s@PLAYWRIGHT_EXPORT.*@PLAYWRIGHT_EXPORT\t$PLAYWRIGHT_EXPORT@g" /opt/semosshome/RDF_Map.prop
+#!/bin/bash
+sed -i "s@PLAYWRIGHT_EXPORT.*@PLAYWRIGHT_EXPORT\t$PLAYWRIGHT_EXPORT@g" /opt/semosshome/RDF_Map.prop
