a8dcbf5112255ce5f5468c96002c44650b4f99e2
kunal0137 <kunal0137@gmail.com>
2026-02-16 16:53:44 -0500

    Add conditional check for PLAYWRIGHT_EXPORT

--- artifacts/scripts/setPlaywrightExport.sh
+++ artifacts/scripts/setPlaywrightExport.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sed -i "s@PLAYWRIGHT_EXPORT.*@PLAYWRIGHT_EXPORT\t$PLAYWRIGHT_EXPORT@g" /opt/semosshome/RDF_Map.prop
+if [ -n "$PLAYWRIGHT_EXPORT" ]; then
+    sed -i "s@PLAYWRIGHT_EXPORT.*@PLAYWRIGHT_EXPORT\t$PLAYWRIGHT_EXPORT@g" /opt/semosshome/RDF_Map.prop
+fi
