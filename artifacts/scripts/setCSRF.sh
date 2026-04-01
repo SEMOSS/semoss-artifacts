49fd1895c7177429d32a0a732185d703e922b03d
Maher Khalil <themaherkhalil@gmail.com>
2026-04-01 12:03:57 -0400

    fix: checking if CORS/CSRF are set to true to enable, updating log4j2

--- artifacts/scripts/setCSRF.sh
+++ artifacts/scripts/setCSRF.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Enable CSRF - activate using $ENABLE_CSRF true
-sed -i '/<!--CSRF_FILTER_FLAG_START/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
-sed -i '/CSRF_FILTER_FLAG_END-->/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+if [[ "${ENABLE_CSRF}" == "true" ]]; then
+    sed -i '/<!--CSRF_FILTER_FLAG_START/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+    sed -i '/CSRF_FILTER_FLAG_END-->/d' $TOMCAT_HOME/webapps/Monolith/WEB-INF/web.xml
+else
+    echo "CSRF is not enabled, skipping..."
+fi
