4152c81d72c4016ec8b6b2b88e609f0aab35741b
Maher Khalil <themaherkhalil@gmail.com>
2026-06-01 21:57:15 -0400

    fix: using environment variables instead of hardcoding java 8 for arm architecture

--- artifacts/dockerBuilder_scripts/install_java.sh
+++ artifacts/dockerBuilder_scripts/install_java.sh
@@ -14,13 +14,12 @@
 ###################################################
 
 arch=$(uname -m)
-if [[ $arch == x86_64* ]]; then
-    echo "X64 Architecture"
+if [[ $arch == arm* ]] || [[ $arch == aarch64 ]]; then
+    echo "ARM Architecture"
+    wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu/bin/zulu${AZUL_ZULU_VERSION}-ca-jdk${JDK_VERSION}-linux_aarch64.tar.gz
+    tar --extract --file /tmp/zulujdk.tar.gz --directory "$JAVA_HOME" --strip-components 1
+else
+    echo "X64 Architecture (default)"
     wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu/bin/zulu${AZUL_ZULU_VERSION}-ca-jdk${JDK_VERSION}-linux_x64.tar.gz
     tar --extract --file /tmp/zulujdk.tar.gz --directory "$JAVA_HOME" --strip-components 1
-elif  [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
-    echo "ARM Architecture"
-    wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu-embedded/bin/zulu8.76.0.17-ca-jdk8.0.402-linux_aarch64.tar.gz
-    tar --extract --file /tmp/zulujdk.tar.gz --directory "$JAVA_HOME" --strip-components 1
-    apt-get install -y openjfx
 fi
